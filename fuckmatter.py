69127d38b5e5c2c8e34780ce8abb8fce2f762136
Timeo Perrinjaquet <99663083+SinisterIcy@users.noreply.github.com>
2023-05-26 19:09:03 +0200

    ⚙ Added args support

--- fuckmatter.py
+++ fuckmatter.py
@@ -3,41 +3,51 @@
  > Code by @SinisterIcy
 '''
 
-# Get the input file
-file = input('BrainFuck file: ')
+import sys
 
-# System variables (do not edit)
-ctnt = ''
-indent_level = 0
-buffer = ''
-final = ''
+# Get the input file by user input if it is not in the args
+if len(sys.argv) == 1:
+	files = [input('BrainFuck file: ')]
+# Get the input files by args
+else:
+	files = sys.argv[1:]
 
-# Indent char (can be replaced with spaces for example)
-indent_char = '\t'
+# Iterate through all the files to format
+for file in files:
+	# System variables (do not edit)
+	ctnt = ''
+	indent_level = 0
+	buffer = ''
+	final = ''
 
-# Get the content of the input file
-with open(file, 'r') as f:
-	ctnt = f.read().replace(' ', '').replace(indent_char, '').replace('\n', '')
-	
-# Initialize the char buffer to the first character
-chbuffer = ctnt[0]
+	# Indent char (can be replaced with spaces for example)
+	indent_char = '\t'
 
-# Iterate through the content
-for i in ctnt:
-	if i == '[':
-		final += indent_char * indent_level + buffer + '\n' + indent_char * indent_level + '['
-		indent_level += 1
-		buffer = ''
-	elif i == ']':
-		final += indent_char * indent_level + buffer + '\n' + indent_char * indent_level + ']'
-		indent_level -= 1
-		buffer = ''
-	elif chbuffer != i:
-		buffer += '\n' + indent_char * indent_level + i
-	else:
-		buffer += i
-	chbuffer = i
+	# Get the content of the input file
+	with open(file, 'r') as f:
+		ctnt = f.read().replace(' ', '').replace(indent_char, '').replace('\n', '')
+		
+	# Initialize the char buffer to the first character
+	chbuffer = ctnt[0]
 
-# Write the result in the file
-with open(file, 'w') as f:
-	f.write(final)
+	# Iterate through the content
+	for i in ctnt:
+		if i == '[':
+			final += indent_char * indent_level + buffer + '\n' + indent_char * indent_level + '['
+			indent_level += 1
+			buffer = ''
+		elif i == ']':
+			final += indent_char * indent_level + buffer + '\n' + indent_char * indent_level + ']'
+			indent_level -= 1
+			buffer = ''
+		elif chbuffer != i:
+			buffer += '\n' + indent_char * indent_level + i
+		else:
+			buffer += i
+		chbuffer = i
+
+	# Write the result in the file
+	with open(file, 'w') as f:
+		f.write(final)
+
+	print(f'✨ Fuckmatter - Formatted "{file}"')
